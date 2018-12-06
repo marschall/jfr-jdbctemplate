b1d1b5d90787b00fcb50e1ddf222c9960d370081
Philippe Marschall <philippe.marschall@netcetera.com>
2018-12-06 16:44:59 +0100

    requires transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module com.github.marschall.jfrjdbctemplate {
   requires spring.beans; // required for tests
   requires spring.core;
   requires spring.tx;
-  requires spring.jdbc;
+  requires transitive spring.jdbc;
 
   exports com.github.marschall.jfrjdbctemplate;
 
